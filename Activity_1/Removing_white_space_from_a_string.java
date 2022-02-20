3766082c4d9c99ee21e212461f99b8b144b99cf7
Lois Gonzales <loisjersong@gmail.com>
2022-02-20 09:58:01 +0800

    added documentation and method

--- Activity_1/Removing_white_space_from_a_string.java
+++ Activity_1/Removing_white_space_from_a_string.java
@@ -4,16 +4,35 @@
 package Activity_1;
 import java.util.Scanner;
 public class Removing_white_space_from_a_string {
+
+    /**
+     *  This method removes all the whitespace in the string
+     * @param string the string which the whitespace will be removed
+     * @return will return the removed whitespace string
+     */
+    public static String removeWhitespace(String string) {
+        String removedSpace = "";
+        for(int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) != ' ') {
+                removedSpace +=  string.charAt(i);
+            } else {
+                continue;
+            }
+        }
+        return removedSpace;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // User will input any character combination
         System.out.print("Enter a String: ");
         String text = input.nextLine();
+        System.out.println(removeWhitespace(text));
 
-        // removed whitespace is text variable and stored it in removeSpace
-        String removedSpace = text.replaceAll("\\s", "");
-        System.out.print("Removed Whitespace: " + removedSpace);
-        
+        // or use .replaceAll(); method to remove whitespace
+        // String removedSpace = text.replaceAll("\\s", "");
+        // System.out.print("Removed Whitespace: " + removedSpace);
+
     }
 }
