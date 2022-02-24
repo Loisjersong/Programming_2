e16239a2ac206581d34063ef39f5900dfc331ae4
Lois Gonzales <loisjersong@gmail.com>
2022-02-24 11:50:12 +0800

    changed countingdupchar algorithm

--- Activity_1/CountingDuplicateCharacters.java
+++ Activity_1/CountingDuplicateCharacters.java
@@ -1,38 +1,73 @@
-/** 
-* @author Gonzales, Lois Jerson A.
-*/
+/**
+ * @author Gonzales, Lois Jerson
+ */
 package Programming_2.Activity_1;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class CountingDuplicateCharacters {
+    /**
+     *  This method will count the duplicate characters in a string
+     *  but it will only work on letter and numbers not on special characters
+     * @param words this parameter will be used to be counted its duplicate
+     */
+    public static void countDuplicate(String words) {
+        int count = 0;
+        System.out.println("char : count");
+            // loop will check if j matches a-z and increment count
+            for(char i = 'A'; i <= 'Z'; i++) {
+                for(int j = 0; j < words.length(); j++) {
+                    if(words.charAt(j) == i)
+                        count++;
+                }
+                // display character and its count
+                if(count > 1) {
+                    System.out.println("  " + i + "  :  " + count);
+                }
+                // reset count to zero
+                count = 0;
+            }
+
+            // loop will check if j matches a-z and increment count
+            for(char i = 'a'; i <= 'z'; i++) {
+                for(int j = 0; j < words.length(); j++) {
+                    if(words.charAt(j) == i)
+                        count++;
+                }
+                // display character and its count
+                if(count > 1) {
+                    System.out.println("  " + i + "  :  " + count);
+                }
+                // reset count to zero
+                count = 0;
+            }
+
+            // loop will check if j matches 0-9 and increment count
+            for(char i = '0'; i <= '9'; i++) {
+                for(int j = 0; j < words.length(); j++) {
+                    if(words.charAt(j) == i)
+                        count++;
+                }
+                
+                // display character and its count
+                if(count > 1) {
+                    System.out.println("  " + i + "  :  " + count);
+                }
+                // reset count to zero
+                count = 0;
+            }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a String: ");
+        // asks user for input
+        System.out.print("Enter a string: ");
         String words = input.nextLine();
-        System.out.println("Duplicated characters: ");
-
-        Map<Character, Integer> countDuplicate = new HashMap<>();
-        char[] charArray = words.toCharArray();
-
-        for(char e: charArray) {
-            if(countDuplicate.containsKey(e)) {
-                countDuplicate.put(e, countDuplicate.get(e) + 1);
-            } else {
-                countDuplicate.put(e, 1);
-            }
-        }
-
-        for (Entry < Character, Integer > entry: countDuplicate.entrySet()) {
-
-            if (entry.getValue() > 1) {
-                System.out.println(entry.getKey() + " : " + entry.getValue());
-            }
-        }
+        System.out.println();
+        
+        // invoke countDuplicate method
+        countDuplicate(words);
 
     }
 }
