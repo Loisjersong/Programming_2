8755c985060dd49417463b863a8e86d4fb04dd2f
Lois Gonzales <loisjersong@gmail.com>
2022-02-28 14:26:35 +0800

    refactor

--- Activity_1/CountingDuplicateCharacters.java
+++ Activity_1/CountingDuplicateCharacters.java
@@ -7,55 +7,25 @@ import java.util.Scanner;
 
 public class CountingDuplicateCharacters {
     /**
-     *  This method will count the duplicate characters in a string
-     *  but it will only work on letter and numbers not on special characters
+     *  This method will count the duplicated characters in a string
      * @param words this parameter will be used to be counted its duplicate
      */
     public static void countDuplicate(String words) {
         int count = 0;
         System.out.println("char : count");
-            // loop will check if j matches a-z and increment count
-            for(char i = 'A'; i <= 'Z'; i++) {
-                for(int j = 0; j < words.length(); j++) {
-                    if(words.charAt(j) == i)
-                        count++;
-                }
-                // display character and its count
-                if(count > 1) {
-                    System.out.println("  " + i + "  :  " + count);
-                }
-                // reset count to zero
-                count = 0;
-            }
 
-            // loop will check if j matches a-z and increment count
-            for(char i = 'a'; i <= 'z'; i++) {
-                for(int j = 0; j < words.length(); j++) {
-                    if(words.charAt(j) == i)
-                        count++;
-                }
-                // display character and its count
-                if(count > 1) {
-                    System.out.println("  " + i + "  :  " + count);
-                }
-                // reset count to zero
-                count = 0;
+        for(char i = 33; i <= 126; i++) {
+            for(int j = 0; j < words.length(); j++) {
+                if(words.charAt(j) == i)
+                    count++;
             }
-
-            // loop will check if j matches 0-9 and increment count
-            for(char i = '0'; i <= '9'; i++) {
-                for(int j = 0; j < words.length(); j++) {
-                    if(words.charAt(j) == i)
-                        count++;
-                }
-                
-                // display character and its count
-                if(count > 1) {
-                    System.out.println("  " + i + "  :  " + count);
-                }
-                // reset count to zero
-                count = 0;
+            // display character and its count
+            if(count > 1) {
+                System.out.println("  " + i + "  :  " + count);
             }
+            // reset count to zero
+            count = 0;
+        }  
     }
 
     public static void main(String[] args) {
